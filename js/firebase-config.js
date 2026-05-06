a038918115db0e53a53fa6a25b32e81d2adf0db1
developereth <developeraddis@proton.me>
2026-05-06 18:07:34 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,12 @@
-// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCAcpb7w7SK_QQPayxiZav638PQQaeDfaU",
   authDomain: "sushiviecatering.firebaseapp.com",
   projectId: "sushiviecatering",
   storageBucket: "sushiviecatering.firebasestorage.app",
   messagingSenderId: "53464789510",
-  appId: "1:53464789510:web:181e18a8e2db525c6bc987",
-  measurementId: "G-8F3WHE183Z"
+  appId: "1:53464789510:web:181e18a8e2db525c6bc987"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
-
-console.log('🔥 Firebase connected successfully!');
