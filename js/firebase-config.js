6af463e3c95fbbf4732f479b0a98f35bd74720b5
developereth <developeraddis@proton.me>
2026-05-07 11:08:03 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCAcpb7w7SK_QQPayxiZav638PQQaeDfaU",
-  authDomain: "sushiviecatering.firebaseapp.com",
-  projectId: "sushiviecatering",
-  storageBucket: "sushiviecatering.firebasestorage.app",
-  messagingSenderId: "53464789510",
-  appId: "1:53464789510:web:181e18a8e2db525c6bc987"
+  apiKey: "AIzaSyBxkCTH1tiqyrWWFaM8MaArdfRBS6wOIQo",
+  authDomain: "sushivibe-7712.firebaseapp.com",
+  projectId: "sushivibe-7712",
+  storageBucket: "sushivibe-7712.firebasestorage.app",
+  messagingSenderId: "502647939325",
+  appId: "1:502647939325:web:8b4e6749a0504d266e1e94"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
+
+console.log('🔥 Firebase connected to sushivibe-7712');
